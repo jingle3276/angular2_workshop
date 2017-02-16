0634ec239ad1d16084c99fb92b623775487e820b
Ying Zhao <jingle3276@hotmail.com>
2017-02-16 13:07:33 -0500

    add widgets component

---
+++ src/app/widgets/widgets.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemsService, Item } from '../shared';
+import { Router, ActivatedRoute } from '@angular/router';
+// import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-widegts',
+  templateUrl: './widgets.component.html',
+  styleUrls: ['./widgets.component.css']
+})
+export class WidgetsComponent { }
+
